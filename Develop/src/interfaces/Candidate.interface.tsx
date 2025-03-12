f8814e2931f4393200fa6a62831e884ba8b94897
Xavier <xaviervang1999@gmail.com>
2025-03-12 15:08:59 -0500

    Added code to Candidateinterface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    id: number;
+    login: string;
+    avatar_url: string;
+    html_url: string;
+    name: string | null;
+    location: string | null;
+    email: string | null;
+    company: string | null;
+  }
