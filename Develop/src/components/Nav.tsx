2617174e0c7f64c12f6d93c11c15e5649ba20a99
Xavier <xaviervang1999@gmail.com>
2025-03-12 15:10:30 -0500

    Added code to Nav in components

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white font-bold text-xl">CandidateSearch</div>
+        <div className="flex space-x-4">
+          <Link to="/" className="text-white hover:text-gray-300">
+            Search
+          </Link>
+          <Link to="/saved" className="text-white hover:text-gray-300">
+            Saved Candidates
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
